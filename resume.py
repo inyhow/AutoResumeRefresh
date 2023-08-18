0c923abf6e20953899a01483ee537c3270bf6ba1
inyhow <inyhow@gmail.com>
2023-08-18 10:15:46 +0800

    1.修复刷新失败问题 2.修复循环无法刷新成功的bug

--- resume.py
+++ resume.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 # Created by Inyhow | python 3.10
+import sys
+
 from selenium.webdriver import Edge
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
@@ -13,11 +15,13 @@ import time
 class AutoRefreshResume:
     def __init__(self):
         # 需要自动刷新简历的网站
-        self.zp_url = 'https://passport.zhaopin.com/login'
-        self.lp_url = 'https://www.liepin.com/'
+        self.zp_url = 'https://i.zhaopin.com/resume'
+        self.lp_url = 'https://c.liepin.com/'
 
     def zhaopin(self):
-        # 实例化Options,为启动无GUI界面做准备
+
+        # 实例化Options,为启动GUI界面做准备
+
         options = Options()
         options.add_experimental_option('useAutomationExtension', False)
         options.add_experimental_option('excludeSwitches', ['enable-automation'])
@@ -26,6 +30,10 @@ class AutoRefreshResume:
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-gpu')
         # options.add_argument("--auto-open-devtools-for-tabs")
+        # 设置自定义的 User-Agent 标头
+        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.200"
+        options.add_argument(f"user-agent={user_agent}")
+
         # 实例化对象
         driver = Edge(options=options)
         driver.maximize_window()
@@ -34,16 +42,21 @@ class AutoRefreshResume:
         # 定位元素，手机扫码，然后进行提交操作
         driver.find_element(By.CLASS_NAME, 'zppp-panel-normal-bar__img').click()
         time.sleep(20)
-        t = driver.find_element(By.CLASS_NAME, "zp-welcome__username").text
+        t = driver.find_element(By.XPATH, "//p[contains(@class,'profile-pre-name')]").text
         if "陈先生" in t:
             print("智联招聘登录成功！\n")
         time.sleep(5)
-        # 等待刷新按钮可见并点击
-        refresh_btn = driver.find_element(By.CLASS_NAME, "refresh__text")
-        time.sleep(3)
-        ActionChains(driver).click(refresh_btn).perform()
+        # 点击刷新
+
+        # 等待刷新简历按钮加载完成并可点击
+        refresh_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//a[@zp-stat-content='刷新简历']"))
+        )
+
+        # 点击刷新简历按钮
+        refresh_button.click()
         # 等待刷新成功提示出现
-        time.sleep(5)
+        time.sleep(1)
         # 等待resume-top元素的出现，设置最长等待时间为10秒
         try:
             element_present = EC.presence_of_element_located((By.CLASS_NAME, "resume-top"))
@@ -52,29 +65,30 @@ class AutoRefreshResume:
         except TimeoutException:
             print("智联招聘简历刷新失败")
         # 打开第二个tab标签访问 猎聘网去执行刷新简历操作
-        driver.execute_script("window.open('https://www.liepin.com/','secondtab');")
+        driver.execute_script("window.open('https://c.liepin.com/','secondtab');")
         # It is switching to second tab now
         driver.switch_to.window("secondtab")
         windowstabs = driver.window_handles
         # 获取当前浏览器的窗口
         currenttab = driver.current_window_handle
         # 切换到新窗口
-        driver.switch_to.window(windowstabs[1])
-        # 定位手机扫码元素
-        driver.find_element(By.CLASS_NAME,  'jsx-3095314988').click()
-        time.sleep(30)
-        driver.find_element(By.CLASS_NAME, "aside-vap-bottom-btn1").click()
-        # 等待元素的出现，设置最长等待时间为10秒
-        try:
-            element_present = EC.presence_of_element_located((By.CLASS_NAME, "refresh-resume-vap-modal-wrap"))
-            WebDriverWait(driver, 10).until(element_present)
-            print("猎聘网刷新简历成功！\n")
-        except TimeoutException:
-            print("元素不存在或超时:猎聘网刷新简历失败")
+        driver.switch_to.window(windowstabs[1]) # 第二个标签页面是猎聘网
 
-        # 进入循环，这个循环从8点到下午5点30,每隔2分钟刷新一次简历
+        # 定位手机扫码元素
+        driver.find_element(By.CLASS_NAME, 'jsx-3095314988').click()
+        time.sleep(30)
+        # 等待刷新简历按钮加载完成并可点击
+        refresh_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//p[text()='刷新简历']"))
+        )
+        # 点击刷新简历按钮
+        refresh_button.click()
+        # 匹配提示信息元素
+        print("猎聘网刷新简历成功！\n")
+
+        # 进入循环，这个循环从8点55到下午5点30,每隔2分钟刷新一次简历
         # 定义开始时间和结束时间
-        start_time = datetime(datetime.now().year, datetime.now().month, datetime.now().day, 8, 0)
+        start_time = datetime(datetime.now().year, datetime.now().month, datetime.now().day, 8, 55)
         end_time = datetime(datetime.now().year, datetime.now().month, datetime.now().day, 17, 30)
         # 初始化计数器
         count = 0
@@ -84,20 +98,22 @@ class AutoRefreshResume:
             count += 1
             # 切换到第一个标签页
             driver.switch_to.window(windowstabs[0])
-
+            driver.refresh()
             # 点击第一个标签页的刷新按钮
-            refresh_btn1 = driver.find_element(By.CLASS_NAME, "refresh__text")
+            refresh_btn1 = driver.find_element(By.XPATH, "//a[@zp-stat-content='刷新简历']")
             ActionChains(driver).click(refresh_btn1).perform()
 
             # 切换到第二个标签页
             driver.switch_to.window(windowstabs[1])
+            driver.refresh()
             time.sleep(5)
             # 点击第二个标签页的刷新按钮
-            driver.find_element(By.CLASS_NAME, "aside-vap-bottom-btn1").click()
+            driver.find_element(By.XPATH, "//p[text()='刷新简历']").click()
             # 输出已经执行的次数
-            print(f"已执行第 {count} 次刷新循环")
+            print(f"已执行第 {count} 次刷新")
             # 等待2分钟
-            time.sleep(120)  # 2分钟等于120秒
+            time.sleep(180)  # 3分钟等于120秒
+        driver.quit
 if __name__ == "__main__":
     job = AutoRefreshResume()
     job.zhaopin()
